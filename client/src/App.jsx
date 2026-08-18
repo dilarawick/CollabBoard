6bfcce666e8c7493a1d5b78ad3ab4208f78c58cc
garfield <inupamauvindi@gmail.com>
2026-08-18 20:52:35 +0530

    Added theme-toggle
    Updated App.jsx, App.css, main.jsx and added themetoggle folder

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,25 @@
-import { Routes, Route } from 'react-router-dom'
-import { routes } from './routes/approutes.js'
-import Header from './components/layout/Header.jsx'
-import Sidebar from './components/layout/Sidebar.jsx'
+import { BrowserRouter } from 'react-router-dom'
+import { FilterProvider } from './context/FilterContext'
+import { TaskProvider } from './context/TaskContext'
+import { UserProvider } from './context/UserContext'
+import { ThemeProvider } from './context/ThemeContext'
+import AppRoutes from './routes/AppRoutes'
+import './App.css'
 
-export default function App() {
+function App() {
   return (
-    <div className="app">
-      <Header />
-      <div className="app-body">
-        <Sidebar />
-        <main className="main">
-          <Routes>
-            {routes.map(({ path, element }) => (
-              <Route key={path} path={path} element={element} />
-            ))}
-          </Routes>
-        </main>
-      </div>
-    </div>
+    <BrowserRouter>
+      <UserProvider>
+        <TaskProvider>
+          <FilterProvider>
+            <ThemeProvider>
+              <AppRoutes />
+            </ThemeProvider>
+          </FilterProvider>
+        </TaskProvider>
+      </UserProvider>
+    </BrowserRouter>
   )
 }
+
+export default App
