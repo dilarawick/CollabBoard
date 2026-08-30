f55e57c13e57e07e85275fdf69e20a18daa9b989
sasini05 <sasini.tm@gmail.com>
2026-08-30 21:52:05 +0530

    Updated tasks controller

--- server/controllers/tasksController.js
+++ server/controllers/tasksController.js
@@ -1,10 +1,13 @@
+// Import tasks service for business logic
 const tasksService = require('../services/tasksService')
 
+// Get all tasks
 function getTasks(req, res) {
   const tasks = tasksService.listTasks()
   res.status(200).json(tasks)
 }
 
+// Create a new task
 function createTask(req, res) {
   try {
     const task = tasksService.addTask(req.body)
@@ -14,6 +17,7 @@ function createTask(req, res) {
   }
 }
 
+// Update an existing task
 function updateTask(req, res) {
   try {
     const task = tasksService.modifyTask(req.params.id, req.body)
@@ -23,6 +27,7 @@ function updateTask(req, res) {
   }
 }
 
+// Delete a task
 function deleteTask(req, res) {
   try {
     tasksService.removeTask(req.params.id)
@@ -32,4 +37,5 @@ function deleteTask(req, res) {
   }
 }
 
+// Export all controller functions
 module.exports = { getTasks, createTask, updateTask, deleteTask }
