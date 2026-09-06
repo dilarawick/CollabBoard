59424de4b8b9c0632c109cd06df93ffb74b303fd
Dilara Wickramanayake <156522027+dilarawick@users.noreply.github.com>
2026-09-06 18:35:23 +0530

    delete dummy files

--- server/seeds/users.js
+++
@@ -1,7 +0,0 @@
-// Seed file: dummy users for local/dev setup.
-// Note: passwordHash values are precomputed for these accounts.
-// Usage example:
-//   const mongoose = require('mongoose');
-//   const User = require('../models/User');
-//   const { users } = require('./index');
-//   await User.insertMany(users);
