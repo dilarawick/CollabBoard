c1ef410d767a092cdc4d8d4d93e4604f6ead1141
Dilara Wickramanayake <156522027+dilarawick@users.noreply.github.com>
2026-09-06 18:34:26 +0530

    delete dummy files

--- server/seeds/boards.js
+++
@@ -1,5 +0,0 @@
-// Seed file: dummy boards for local/dev setup.
-// Usage example:
-//   const Board = require('../models/Board');
-//   const { boards } = require('./index');
-//   await Board.insertMany(boards);
